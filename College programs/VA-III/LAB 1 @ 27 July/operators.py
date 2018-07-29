afa7169ae33c51bb9699676765cdaacaf13e411b
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-07-29 12:43:36 +0530

    added VA

---
+++ College programs/VA-III/LAB 1 @ 27 July/operators.py
@@ -0,0 +1,15 @@
+i1 = 2
+i2 = 3
+i3 = -5
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+print(i1+(i2+i3))
+print(i1*(i2+i3))
+print(i1/(i2*i3))
+print(i1//(i2+i3))
+print(i1/(i2+i3))
+print(d1+(d2*d3))
+print(d1/d2-d3)
+print((d1+d2+d3)/3)
